69306dc0522da77aa35162cb6d5c663d737aa5c0
Simon Condon <simoncondon@hotmail.com>
2019-03-09 16:21:26 +0000

    A couple of improvements: ..
    * Added a Directory.Build.props, moved common props from individual projects into it.
    * Added Views.Renderables.Primitives class library.

---
+++ src/Views.Renderables.Primitives/2d/PrimitiveVertex.cs
@@ -0,0 +1,16 @@
+﻿namespace GLHDN.Views.Renderables.Primitives._2d
+{
+    using System.Numerics;
+
+    public struct PrimitiveVertex
+    {
+        public readonly Vector3 Position;
+        public readonly Vector4 Color;
+
+        public PrimitiveVertex(Vector3 position, Vector4 color)
+        {
+            this.Position = position;
+            this.Color = color;
+        }
+    }
+}
