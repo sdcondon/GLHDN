2b597c9f98f7049a637bd1e47eb8706c68ebc42e
Simon Condon <simoncondon@hotmail.com>
2019-03-12 22:03:49 +0000

    Added global directed light to primitive renderer. Also fixed primitive cuboid.

--- src/Views.Renderables.Primitives/Colored.Vertex.glsl
+++ src/Views.Renderables.Primitives/Colored.Vertex.glsl
@@ -8,15 +8,16 @@ layout(location = 2) in vec3 vertexNormal_modelspace;
 // Output data ; will be interpolated for each fragment.
 out vec3 matColor;
 out vec3 Position_worldspace;
+out vec3 Normal_worldspace;
 out vec3 Normal_cameraspace;
 out vec3 EyeDirection_cameraspace;
-out vec3 LightDirection_cameraspace;
+out vec3 PointLightDirection_cameraspace;
 
 // Values that stay constant for the whole mesh.
 uniform mat4 MVP;
 uniform mat4 V;
 uniform mat4 M;
-uniform vec3 LightPosition_worldspace;
+uniform vec3 PointLightPosition_worldspace;
 
 void main(){
 
@@ -32,12 +33,14 @@ void main(){
 	EyeDirection_cameraspace = vec3(0, 0, 0) - vertexPosition_cameraspace;
 
 	// Vector that goes from the vertex to the light, in camera space. M is omited because it's identity.
-	vec3 LightPosition_cameraspace = (V * vec4(LightPosition_worldspace, 1)).xyz;
-	LightDirection_cameraspace = LightPosition_cameraspace + EyeDirection_cameraspace;
+	vec3 PointLightPosition_cameraspace = (V * vec4(PointLightPosition_worldspace, 1)).xyz;
+	PointLightDirection_cameraspace = PointLightPosition_cameraspace + EyeDirection_cameraspace;
 	
 	// Normal of the the vertex, in camera space
 	Normal_cameraspace = (V * M * vec4(vertexNormal_modelspace, 0)).xyz; // Only correct if M does not scale the model! Use its inverse transpose if not.
-	
+
+	Normal_worldspace = (M * vec4(vertexNormal_modelspace, 0)).xyz;
+
 	// Color of the vertex. No special space for this one.
 	matColor = vertexColor;
 }
