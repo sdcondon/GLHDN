0d606242bbbaadcab168853bdfb7bec96a2f3802
Simon Condon <simoncondon@hotmail.com>
2019-04-15 21:36:59 +0100

    Couple of tweaks
    * Added support for transparency to primitive shader
    * Added some extension methods to allow primitives specification to be a little more succinct.

--- src/Views.Renderables.Primitives/Colored.Vertex.glsl
+++ src/Views.Renderables.Primitives/Colored.Vertex.glsl
@@ -2,11 +2,11 @@
 
 // Input vertex data, different for all executions of this shader.
 layout(location = 0) in vec3 vertexPosition_modelspace;
-layout(location = 1) in vec3 vertexColor;
+layout(location = 1) in vec4 vertexColor;
 layout(location = 2) in vec3 vertexNormal_modelspace;
 
 // Output data ; will be interpolated for each fragment.
-out vec3 matColor;
+out vec4 matColor;
 out vec3 Position_worldspace;
 out vec3 Normal_worldspace;
 out vec3 Normal_cameraspace;
