c2ffb6d351765b0d0450faabfebc0f1c40548622
Simon Condon <simoncondon@hotmail.com>
2019-05-16 22:19:03 +0100

    A few minor tweaks (#19)
    * Fixed cursor locking in example app.
    * ReactiveBuffersTests fix - now tests that the order of data in the resultant buffer is correct
    * Removed unused using from Font class
    * Comment correction in IElementParent

--- src/Views.Renderables.Gui/IElementParent.cs
+++ src/Views.Renderables.Gui/IElementParent.cs
@@ -14,12 +14,12 @@
         IObservable<IObservable<Element>> SubElements { get; }
 
         /// <summary>
-        /// Gets the position of the center of the element, in screen space.
+        /// Gets the position of the center of this object, in screen space.
         /// </summary>
         Vector2 Center { get; }
 
         /// <summary>
-        /// Gets the size of the element, in screen space (i.e. pixels).
+        /// Gets the size of this object, in screen space (i.e. pixels).
         /// </summary>
         Vector2 Size { get; }
     }
