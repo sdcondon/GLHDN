5536c5fb2f5315e8cb96b1f09e81989fcdcd1f27
Simon Condon <simoncondon@hotmail.com>
2019-06-26 22:47:46 +0100

    Factored example app rendering code into renderable class

--- src/ExampleApps.WinForms/Program.cs
+++ src/ExampleApps.WinForms/Program.cs
@@ -14,16 +14,6 @@
 
     static class Program
     {
-        private static Views.View view;
-        private static ICamera camera;
-
-        private static ColoredLines lines;
-        private static Gui gui;
-        private static TextElement camText;
-
-        private static Matrix4x4 cubeWorldMatrix = Matrix4x4.Identity;
-        private static Subject<IList<Primitive>> cubeSubject = new Subject<IList<Primitive>>();
-
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -35,95 +25,109 @@
 
             var form = new GlForm()
             {
-                WindowState = FormWindowState.Normal,
-                FormBorderStyle = FormBorderStyle.Sizable
+                // WindowState = FormWindowState.Normal,
+                // FormBorderStyle = FormBorderStyle.Sizable
             };
 
-            view = new Views.View(form.ViewContext, true, Vector3.Zero);
-            view.Update += ModelUpdate;
+            var view = new Views.View(form.ViewContext, true, Color.Black());
 
-            camera = new FirstPersonCamera(
-                movementSpeed: 3.0f,
-                rotationSpeed: 0.005f,
-                fieldOfViewRadians: (float)Math.PI / 4.0f,
-                nearPlaneDistance: 0.1f,
-                farPlaneDistance: 100f,
-                initialPosition: new Vector3(0f, 0f, 3f),
-                initialHorizontalAngleRadians: (float)Math.PI,
-                initialVerticalAngleRadians: 0f);
-
-            view.Renderables.Add(new TexturedStaticMesh(
-                camera,
-                new[]
-                {
-                    new TexturedStaticMesh.Vertex(
-                        new Vector3(-1f, -1f, -2f),
-                        new Vector2(0f, 0f),
-                        new Vector3(0f, 0f, 1f)),
-                    new TexturedStaticMesh.Vertex(
-                        new Vector3(1f, -1f, -2f),
-                        new Vector2(1f, 0f),
-                        new Vector3(0f, 0f, 1f)),
-                    new TexturedStaticMesh.Vertex(
-                        new Vector3(0f, 1f, -2f),
-                        new Vector2(0.5f, 1f),
-                        new Vector3(0f, 0f, 1f)),
-                },
-                new uint[] { 0, 1, 2 },
-                "uvmap.DDS"));
-
-            view.Renderables.Add(new PrimitiveRenderer(camera, Observable.Return(cubeSubject))
-            {
-                AmbientLightColor = Color.Grey(0.1f),
-                DirectedLightDirection = new Vector3(0, 1f, 0f),
-                DirectedLightColor = Color.Grey()
-            });
-
-            view.Renderables.Add(Program.lines = new ColoredLines(camera));
-
-            Program.camText = new TextElement(
-                new Layout((-1f, 1f), (-1f, 1f), (1f, 0f)),
-                color: Color.White());
-            view.Renderables.Add(gui = new Gui(view)
-            {
-                SubElements =
-                {
-                    new PanelElement(
-                        layout: new Layout((-1f, 0f), (-1f, 0f), (250, 1f)),
-                        color: Color.White(0.05f),
-                        borderWidth: 0f)
-                    {
-                        SubElements =
-                        {
-                            camText,
-                            new Views.Renderables.Gui.Button(
-                                layout: new Layout((1f, 1f), (1f, 1f), (100, 100)),
-                                color: Color.Blue(),
-                                textColor: Color.White(),
-                                text: "Dispose",
-                                (s, e) => view.Dispose())
-                        }
-                    }
-                }
-            });
+            var demo = new DemoRenderable(view);
+            view.Renderables.Add(demo);
+            view.Update += demo.Update;
 
             Application.Run(form);
         }
 
-        private static void ModelUpdate(object sender, TimeSpan elapsed)
+        private class DemoRenderable : CompositeRenderable
         {
-            camera.Update(elapsed, view);
-            gui.Update();
-            camText.Content = $"Hello, world!\n\nCam: {camera.Position:F2}";
+            private readonly ICamera camera;
 
-            cubeWorldMatrix *= Matrix4x4.CreateRotationZ((float)elapsed.TotalSeconds);
-            cubeWorldMatrix *= Matrix4x4.CreateRotationY((float)elapsed.TotalSeconds / 2);
-            cubeSubject.OnNext(new[] { Primitive.Cuboid(new Vector3(.5f, 1f, 0.75f), cubeWorldMatrix, Color.Red()) });
+            private readonly ColoredLines lines;
+            private readonly Gui gui;
+            private readonly TextElement camTextElement;
 
-            if (view.WasLeftMouseButtonReleased)
+            private Matrix4x4 cubeWorldMatrix = Matrix4x4.Identity;
+            private Subject<IList<Primitive>> cubeSubject = new Subject<IList<Primitive>>();
+
+            public DemoRenderable(Views.View view)
             {
-                var ray = new Ray(camera, view);
-                lines.AddLine(ray.Origin, ray.Origin + ray.Direction * 10);
+                camera = new FirstPersonCamera(
+                    movementSpeed: 3.0f,
+                    rotationSpeed: 0.005f,
+                    fieldOfViewRadians: (float)Math.PI / 4.0f,
+                    nearPlaneDistance: 0.1f,
+                    farPlaneDistance: 100f,
+                    initialPosition: new Vector3(0f, 0f, 3f),
+                    initialHorizontalAngleRadians: (float)Math.PI,
+                    initialVerticalAngleRadians: 0f);
+
+                Renderables.Add(new TexturedStaticMesh(
+                    camera,
+                    new[]
+                    {
+                        new TexturedStaticMesh.Vertex(
+                            new Vector3(-1f, -1f, -2f),
+                            new Vector2(0f, 0f),
+                            new Vector3(0f, 0f, 1f)),
+                        new TexturedStaticMesh.Vertex(
+                            new Vector3(1f, -1f, -2f),
+                            new Vector2(1f, 0f),
+                            new Vector3(0f, 0f, 1f)),
+                        new TexturedStaticMesh.Vertex(
+                            new Vector3(0f, 1f, -2f),
+                            new Vector2(0.5f, 1f),
+                            new Vector3(0f, 0f, 1f)),
+                    },
+                    new uint[] { 0, 1, 2 },
+                    "uvmap.DDS"));
+
+                Renderables.Add(new PrimitiveRenderer(camera, Observable.Return(cubeSubject))
+                {
+                    AmbientLightColor = Color.Grey(0.1f),
+                    DirectedLightDirection = new Vector3(0, 1f, 0f),
+                    DirectedLightColor = Color.Grey()
+                });
+
+                Renderables.Add(lines = new ColoredLines(camera));
+
+                camTextElement = new TextElement(
+                    new Layout((-1f, 1f), (-1f, 1f), (1f, 0f)),
+                    color: Color.White());
+                Renderables.Add(gui = new Gui(view)
+                {
+                    SubElements =
+                    {
+                        new PanelElement(
+                            layout: new Layout((-1f, 0f), (-1f, 0f), (250, 1f)),
+                            color: Color.White(0.05f),
+                            borderWidth: 0f)
+                        {
+                            SubElements =
+                            {
+                                camTextElement,
+                            },
+                        },
+                    },
+                });
+            }
+
+            public void Update(object sender, TimeSpan elapsed)
+            {
+                var view = (Views.View)sender;
+
+                camera.Update(elapsed, view);
+                gui.Update();
+                camTextElement.Content = $"Hello, world!\n\nCam: {camera.Position:F2}";
+
+                cubeWorldMatrix *= Matrix4x4.CreateRotationZ((float)elapsed.TotalSeconds);
+                cubeWorldMatrix *= Matrix4x4.CreateRotationY((float)elapsed.TotalSeconds / 2);
+                cubeSubject.OnNext(new[] { Primitive.Cuboid(new Vector3(.5f, 1f, 0.75f), cubeWorldMatrix, Color.Red()) });
+
+                if (view.WasLeftMouseButtonReleased)
+                {
+                    var ray = new Ray(camera, view);
+                    lines.AddLine(ray.Origin, ray.Origin + ray.Direction * 10);
+                }
             }
         }
     }
