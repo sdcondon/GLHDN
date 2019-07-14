4f2c069ff24efc79278a1120b79b0f20e5848def
Simon Condon <simoncondon@hotmail.com>
2019-07-14 13:36:39 +0100

    Added LoadBMP method to TextureLoader class.

--- src/Core/IO/TextureLoader.cs
+++ src/Core/IO/TextureLoader.cs
@@ -7,14 +7,10 @@
     using System.Text;
 
     /// <summary>
-    /// Simple DDS loader nabbed from an OpenGL tutorial.
+    /// Simple texture loader nabbed from an OpenGL tutorial.
     /// </summary>
     public static class TextureLoader
     {
-        private const uint FOURCC_DXT1 = 0x31545844; // Equivalent to "DXT1" in ASCII
-        private const uint FOURCC_DXT3 = 0x33545844; // Equivalent to "DXT3" in ASCII
-        private const uint FOURCC_DXT5 = 0x35545844; // Equivalent to "DXT5" in ASCII
-
         private static readonly Dictionary<string, InternalFormat> DDSPixelFormats = new Dictionary<string, InternalFormat>()
         {
             { "DXT1", InternalFormat.CompressedRgbaS3tcDxt1Ext },
@@ -25,16 +21,16 @@
         /// <summary>
         /// Loads a DDS image from a given file path.
         /// </summary>
-        /// <param name="imagepath">The file path to load the image from.</param>
-        /// <returns>The Open GL texture ID that the image has been loaded into.</returns>
-        public static uint LoadDDS(string imagepath)
+        /// <param name="filePath">The file path to load the image from.</param>
+        /// <returns>The OpenGL texture ID that the image has been loaded into.</returns>
+        public static uint LoadDDS(string filePath)
         {
             uint height;
             uint width;
             uint mipMapCount;
             InternalFormat format;
             byte[] buffer;
-            using (var file = File.Open(imagepath, FileMode.Open))
+            using (var file = File.Open(filePath, FileMode.Open))
             {
                 // Read file header
                 var header = new byte[128];
@@ -42,7 +38,7 @@
 
                 if (Encoding.ASCII.GetString(header, 0, 4) != "DDS ")
                 {
-                    throw new ArgumentException("Specified file is not a DDS file", nameof(imagepath));
+                    throw new ArgumentException("Specified file is not a DDS file", nameof(filePath));
                 }
 
                 height = BitConverter.ToUInt32(header, 12);
@@ -52,7 +48,7 @@
                 var fourCC = Encoding.ASCII.GetString(header, 84, 4);
                 if (!DDSPixelFormats.TryGetValue(Encoding.ASCII.GetString(header, 84, 4), out format))
                 {
-                    throw new ArgumentException($"Specified file uses unsupported internal format {fourCC}", nameof(imagepath));
+                    throw new ArgumentException($"Specified file uses unsupported internal format {fourCC}", nameof(filePath));
                 }
 
                 //uint components = (fourCC == FOURCC_DXT1) ? 3u : 4u;
@@ -63,7 +59,7 @@
                 file.Read(buffer, 0, bufferSize);
             }
 
-	        // Create OpenGL texture
+            // Create OpenGL texture
             var textureId = Gl.GenTexture();
             Gl.BindTexture(TextureTarget.Texture2d, textureId); // "Bind" the newly created texture: all future texture functions will modify this texture
             Gl.PixelStore(PixelStoreParameter.UnpackAlignment, 1);
@@ -71,24 +67,81 @@
             // Load the mipmaps
             uint blockSize = (format == InternalFormat.CompressedRgbaS3tcDxt1Ext) ? 8u : 16u;
             uint offset = 0;
-            for (int level = 0; level < mipMapCount && (width > 0 || height > 0); ++level) 
-	        { 
-		        uint levelSize = ((width + 3) / 4) * ((height + 3) / 4) * blockSize;
+            for (int level = 0; level < mipMapCount && (width > 0 || height > 0); ++level)
+            {
+                uint levelSize = ((width + 3) / 4) * ((height + 3) / 4) * blockSize;
 
                 var levelBuffer = new byte[levelSize];
                 Array.Copy(buffer, offset, levelBuffer, 0, levelSize);
                 Gl.CompressedTexImage2D(TextureTarget.Texture2d, level, format, (int)width, (int)height, 0, (int)levelSize, levelBuffer);
 
-                offset += levelSize; 
-		        width /= 2; 
-		        height /= 2; 
+                offset += levelSize;
+                width /= 2;
+                height /= 2;
 
-		        // Deal with non-power-of-two textures. This code is not included in the webpage to reduce clutter.
-		        if (width < 1) width = 1;
-		        if (height < 1) height = 1;
-	        } 
+                // Deal with non-power-of-two textures. This code is not included in the webpage to reduce clutter.
+                if (width < 1) width = 1;
+                if (height < 1) height = 1;
+            }
 
-	        return textureId;
+            return textureId;
+        }
+
+        /// <summary>
+        /// Loads a (24bpp) BMP image from a given file path.
+        /// </summary>
+        /// <param name="filePath">The file path to load the image from.</param>
+        /// <returns>The OpenGL texture ID that the image has been loaded into.</returns>
+        public static uint LoadBMP(string filePath)
+        {
+            uint dataPos; // Position in the file where the actual data begins
+            uint width, height;
+            uint imageSize; // = width*height*3           
+            byte[] data; // Actual RGB data
+            using (var file = File.Open(filePath, FileMode.Open))
+            {
+                // Read file header
+                var header = new byte[54];
+                file.Read(header, 0, 54);
+
+                if (Encoding.ASCII.GetString(header, 0, 2) != "BM")
+                {
+                    throw new ArgumentException("Specified file is not a BMP file", nameof(filePath));
+                }
+
+                dataPos = BitConverter.ToUInt32(header, 10);
+                width = BitConverter.ToUInt32(header, 18);
+                height = BitConverter.ToUInt32(header, 22);
+                imageSize = BitConverter.ToUInt32(header, 34);
+
+                // Some BMP files are misformatted, guess missing information
+                if (imageSize == 0) imageSize = width * height * 3; // 3 : one byte for each Red, Green and Blue component
+                if (dataPos == 0) dataPos = 54; // The BMP header is done that way
+
+                // Create a buffer
+                data = new byte[imageSize];
+                file.Read(data, 0, (int)imageSize);
+            }
+
+            var textureId = Gl.GenTexture();
+            Gl.BindTexture(TextureTarget.Texture2d, textureId); // "Bind" the newly created texture : all future texture functions will modify this texture
+
+            // Give the image to OpenGL
+            Gl.TexImage2D(
+                TextureTarget.Texture2d,
+                0,
+                InternalFormat.Rgb,
+                (int)width,
+                (int)height,
+                0,
+                PixelFormat.Bgr,
+                PixelType.UnsignedByte,
+                data);
+
+            Gl.TexParameteri(TextureTarget.Texture2d, TextureParameterName.TextureMagFilter, Gl.NEAREST);
+            Gl.TexParameteri(TextureTarget.Texture2d, TextureParameterName.TextureMinFilter, Gl.NEAREST);
+
+            return textureId;
         }
     }
 }
